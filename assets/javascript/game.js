8138f3b0d49bf7ab0c4aaa8515aa853bfe76d777
ntdang <ndang83@gmail.com>
2018-02-09 15:26:01 -0500

    Array shows

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@
     var winsHTML = document.getElementById('wins');
     var lossesHTML = document.getElementById('losses');
     var guessesLeftHTML = document.getElementById('guesses-left');
-    var userGuessHTML = document.getElementById('user-guess');
+    var guessesSoFarHTML = document.getElementById('guesses-sofar');
 
     // Create a variable array to hold possible computer choices
     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -13,12 +13,12 @@
     var wins = 0;
     var losses = 0;
     var guessesLeft = 10;
-    var userGuess = '';
+    var guessesSoFar = [];
 
     //Create reset function
     var reset = function() {
       guessesLeft = 10;
-      userGuess = [];
+      guessesSoFar = [];
     }
 
     // Whenever a key is pressed, alert "pressed a button".
@@ -31,8 +31,10 @@
       var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
       console.log(computerChoice);
 
+      guessesSoFar.push(userGuess);
+
       // Check user's guess is a letter
-      // if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+
         // Let's run the rest of the game
 
       if (userGuess === computerChoice) {
@@ -48,28 +50,11 @@
         reset();
       }
 
-
-        // if ((userGuess === "r") && (computerGuess === "s")) {
-        //   wins++;
-        // } else if ((userGuess === "r") && (computerGuess === "p")) {
-        //   losses++;
-        // } else if ((userGuess === "s") && (computerGuess === "r")) {
-        //   losses++;
-        // } else if ((userGuess === "s") && (computerGuess === "p")) {
-        //   wins++;
-        // } else if ((userGuess === "p") && (computerGuess === "s")) {
-        //   losses++;
-        // } else if ((userGuess === "p") && (computerGuess === "r")) {
-        //   wins++;
-        // } else if (userGuess === computerGuess) {
-        //   ties++;
-        // }
-
         // Write results to HTML
         winsHTML.textContent = wins;
         lossesHTML.textContent = losses;
         guessesLeftHTML.textContent = guessesLeft;
-        userGuessHTML.textContent = userGuess;
+        guessesSoFarHTML.textContent = guessesSoFar;
         //Need to display list of guessed letters
       
 
